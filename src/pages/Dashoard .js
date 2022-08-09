fbe074c3c80b7eeba901d2bea7ff64741197d3a3
unknown <k1013726@gmail.com>
2022-08-09 12:00:25 +0800

    0809 issues commit

--- src/pages/Dashoard .js
+++ src/pages/Dashoard .js
@@ -1,5 +1,22 @@
-const Dashoard  = () => {
-    return <h1>Dashoard </h1>;
-  };
-  
-  export default Dashoard ;
+import '../style/Dashoard.css'
+import map from  '../img/mao.png'
+
+const Dashoard = () => {
+  return (
+    
+    <div className="DivContainer ">
+      <div className="DivContainer2">
+        <div className="borderTest Fontsytle">company info</div>
+        <div className="borderTest Mapstyle">2</div>
+      </div>
+      <div className="DivContainer2">
+        <div className="borderTest Fontsytle">company locations</div>
+        <div className="borderTest"><img src={map} alt='' className="Mapstyle"></img></div>
+      </div>
+    </div>
+
+
+  );
+};
+
+export default Dashoard;
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,20 +1,25 @@
 import { Outlet, Link } from "react-router-dom";
 import '../style/layout.css'
-// import img from '../../img/Logo.png'
+import Logo from '../img/Logo.png'
+import cradit from '../img/cradit.png'
 
 const Layout = () => {
     return (
         <>
             <nav >
                 <div className="sticky_header">
-                    <diV className="borderTest">
+                    <diV className="borderTest Logo">
+                        <img src={Logo} alt="none"></img>
                     </diV>
-                    <diV className="borderTest">
-                        <Link to="/" className="wordsytle frame">Dashoard </Link>
+                    <diV className="borderTest ">
+                        <Link to="/" className="wordsytle frame" >Dashoards</Link>
                         <Link to="/Stacking" className="wordsytle frame">Stacking</Link>
                         <Link to="/Market" className="wordsytle frame">Market Place</Link>
                     </diV>
-                    <diV className="borderTest"></diV>
+                    <diV className="borderTest ">
+                        <img src={cradit} alt="none" className="credit"></img>
+                        <div className="creditfont">10,279 USD</div>
+                    </diV>
 
 
                 </div>
